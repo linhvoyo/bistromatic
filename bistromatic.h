1fbfc202b715bf88b3bd4325a86b048d92ad8bfc
Hiroshi Usui <hiroshiusui@Mac-Admin.local>
2018-01-10 14:34:16 -0800

    ft_itoa.c

--- bistromatic.h
+++ bistromatic.h
@@ -6,7 +6,7 @@
 /*   By: hiroshiusui <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 18:41:07 by hiroshius         #+#    #+#             */
-/*   Updated: 2018/01/10 13:26:14 by hiroshius        ###   ########.fr       */
+/*   Updated: 2018/01/10 14:29:24 by hiroshius        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,10 @@ typedef struct              s_linked_list
 
 char *add(char *s1, char *s2);
 char *subtract(char *s1, char *s2);
+void truncate_zeros(char **str);
 
-char *ft_strcpy(char *dst, char *src)
+char *ft_itoa(int n);
+char *ft_strcpy(char *dst, char *src);
 char *ft_strdup(char *s1);
 char *ft_strnew(int size);
 int ft_strlen(char *str);
