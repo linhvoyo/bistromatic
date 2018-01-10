0375284ec633fdd76871d0c5ba6275ba59b3267e
Hiroshi Usui <hiroshiusui@Mac-Admin.local>
2018-01-10 12:24:56 -0800

    Add prototypes for add and subtract

--- bistromatic.h
+++ bistromatic.h
@@ -6,7 +6,7 @@
 /*   By: hiroshiusui <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 18:41:07 by hiroshius         #+#    #+#             */
-/*   Updated: 2018/01/10 12:08:23 by hiroshius        ###   ########.fr       */
+/*   Updated: 2018/01/10 12:24:17 by hiroshius        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,7 @@ typedef struct              s_linked_list
     char                    *data;
     struct s_linked_list    *next;
 }                           linked_list;
+
+char *add(char *s1, char *s2);
+char *subtract(char *s1, char *s2);
 #endif
