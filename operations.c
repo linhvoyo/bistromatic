5b79570eca4157147d700c67d79f388abf4923f7
Linh Lam <lilam@e1z2r4p25.42.us.org>
2018-01-10 06:35:44 -0800

    test1

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: lilam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 07:58:23 by lilam             #+#    #+#             */
-/*   Updated: 2018/01/09 12:06:15 by lilam            ###   ########.fr       */
+/*   Updated: 2018/01/09 17:20:42 by lilam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,8 +140,8 @@ char *subtraction(char *s1, char *s2)
 	int len2;
 	int carry;
 	int small;
-
 	int big;
+	int i;
 
 	big = 0;
 	carry = 0;
@@ -162,29 +162,51 @@ char *subtraction(char *s1, char *s2)
 	if (!total)
 		return (NULL);
 	
-	printf("%d\n", big);
-	printf("%d\n", small);
+	printf("%s %d\n", s1 ,big);
+	printf("%s %d\n", s2, small);
 
-/*	
+	i = big;
+	printf("i %d\n", i);
+
 	total[i--] = '\0';
 	while (i > 0)
 	{
-		int value1 = s1[i - 1] - '0';
-		int value2 = s2[i - 1] - '0';
+		int value1 = s1[i] - '0';
+		int value2 = s2[i] - '0';
+		printf("value 1: %d, value 2: %d\n", value1, value2);
 		if ((value1) - (value2) < 0)
 		{
+			puts("hi");
 			total[i] = (value1 + 10) - (value2) + carry + '0';
 			carry = -1;
+			if (i == 1)
+				total[0] = '0';
 		}
 		else
 		{
-			total[i] = (value1) - (value2) + carry + '0';
+			if (carry == -1 && value1 - value2 == 0)
+			{
+				puts("there");
+				total[i] = 10 - value1 - value2 + carry + '0';
+				carry = -1;
+			}
+			else
+			{
+				puts("asdfadsf");
+				printf("%d\n", carry);
+				printf("value 1: %d, value 2: %d\n", value1, value2);
+				total[i] = (value1) - (value2) + carry + '0';
+			}
 			carry = 0;
+//			if (i == 1)
+//				total[0] = '9';
 		}
+		printf("%c total[%d] \n", total[i], i );
 		i--;
 	}
 
-*/
+	puts(total);
+
 	return ("sdfs");	
 }
 
@@ -204,7 +226,8 @@ char *multiplication(char *s1, char *s2)
 int main()
 {
 //	printf("%s\n", addition("15", "80"));
-	printf("%s\n", subtraction("5", "10"));
+	printf("%s\n", subtraction("1000090", "644"));
+//	printf("%s\n", subtraction("3000546090", "56445644"));
 //	printf("%s\n", multiplication("15", "80"));
 //	addition("5654654", "32132564154");
 	return (0);
