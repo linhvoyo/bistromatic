dc04031c4b8769bf39eae80d629e8a0e9ebf9872
Hiroshi Usui <hiroshiusui@Mac-Admin.local>
2018-01-11 22:21:43 -0800

    Stuff

--- modulo.c
+++ modulo.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pad_left.c                                         :+:      :+:    :+:   */
+/*   modulo.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hiroshiusui <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/11 09:55:38 by hiroshius         #+#    #+#             */
-/*   Updated: 2018/01/11 09:56:00 by hiroshius        ###   ########.fr       */
+/*   Created: 2018/01/11 22:05:38 by hiroshius         #+#    #+#             */
+/*   Updated: 2018/01/11 22:10:29 by hiroshius        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bistromatic.h"
 
-char *pad_left(char *s1, int n)
+char *modulo(char *dividend, char *divisor)
 {
-    char *str;
-    int len;
-    int i;
-    int j;
-
-    len = ft_strlen(s1);
-    str = (char*)malloc(sizeof(char)*(n + 1));
-    if (!str)
-        return (0);
-    i = 0;
-    j = 0;
-    while (i < (n - len))
-        str[i++] = '0';
-    while (i < n)
-        str[i++] = s1[j++];
-    str[n] = '\0';
-    return (str);
+	if (number_cmp(dividend, divisor) < 0)
+		return (divisor);
+	return (dividend);
 }
