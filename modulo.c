c8c15b8f3ff824d8815c1f59951a6af5ede0e8fe
Hiroshi Usui <hiroshiusui@Mac-Admin.local>
2018-01-12 16:52:29 -0800

    Change dumb modulo to smart modulo, add 99999/33 unit test.

--- modulo.c
+++ modulo.c
@@ -9,20 +9,17 @@ char *modulo(char *dividend, char *divisor)
 	if (zeroed(divisor))
 		return ("Error: Division by 0.");
 	negative_flag = "";
-	if (number_cmp(dividend, "0") < 0)
+	if (is_negative(dividend))
 		dividend++;
-	if (number_cmp(divisor, "0") < 0 && (negative_flag = "-"))
+	if (is_negative(divisor) && (negative_flag = "-"))
 		divisor++;
 	if (number_cmp(dividend, divisor) < 0)
 		return (divisor);
-	multiplier = ft_strdup("0");
-	while (number_cmp(dividend, multiply(multiplier, divisor)) > 0)
-		multiplier = add(multiplier, "1");
-	if (number_cmp(dividend, multiply(multiplier, divisor)) < 0)
-		result = multiply(subtract(multiplier, "1"), divisor);
-	else
-		result = multiply(multiplier, divisor);
+	multiplier = divide(dividend, divisor);
+	result = multiply(multiplier, divisor);
 	if (number_cmp(result, dividend) == 0)
 		return ("0");
+	if (number_cmp(result, dividend) < 0)
+		return (subtract(dividend, result));
 	return (ft_strjoin(negative_flag, subtract(dividend, result)));
 }
