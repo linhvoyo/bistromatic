1fa23b2e0c15fdfcc5fa175d7ae04b129ebd0a30
Hiroshi Usui <hiroshiusui@Mac-Admin.local>
2018-01-12 16:30:38 -0800

    Export truncate_zeros.c

---
+++ truncate_zeros.c
@@ -0,0 +1,25 @@
+#include "bistromatic.h"
+
+void truncate_zeros(char **str)
+{
+	int i;
+	int sign;
+
+	char *temp;
+
+	i = 0;
+	if (*str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while ((*str)[i] && (*str)[i] == '0')
+		i++;
+	temp = *str;
+	if (sign == -1)
+		*str = ft_strjoin("-", *str + i);
+	else
+		*str = ft_strdup(*str + i);
+	free(temp);
+}
+
